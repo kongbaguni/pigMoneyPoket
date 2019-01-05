60329fc62f015323087b8fa63275bd30cf847d5e
Changyul Seo <kongbaguni@gmail.com>
2019-01-05 18:47:24 +0900

    Initial Commit

---
+++ PigMoneyPoket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PigMoneyPoket
+//
+//  Created by Changyul Seo on 05/01/2019.
+//  Copyright © 2019 Changyul Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
